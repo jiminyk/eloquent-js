26cb2007c5388d92c846e0583e2498d48dd96b44
Christian Gomez <cgomez@xixmia.com>
2019-06-13 22:03:01 -0400

    Solved triangle loop excercise

---
+++ 02_ProgramStructure/Excercises/triangle.js
@@ -0,0 +1,23 @@
+
+// let state = [];
+
+// state.push('#');
+
+// for (number = 0; number <= 7; number += 1) {
+//     console.log(number);
+// }
+
+// solution:
+for(let string = '#'; string.length < 8; string += '#'){
+    console.log(string);
+}
+
+// Prints
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
